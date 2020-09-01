fd1a6d67face98572311260dfe1932ff31a0bb5b
hackycy <qa894178522@qq.com>
2020-09-01 18:17:17 +0800

    fix：修改默认配置

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'Vue Element Admin',
+  title: '思忆管理后台',
 
   /**
    * @type {boolean} true | false
@@ -31,5 +31,5 @@ module.exports = {
    * The default is only used in the production env
    * If you want to also use it in dev, you can pass ['production', 'development']
    */
-  errorLog: 'production'
+  errorLog: 'development'
 }
