4d42a0b5143984bd62af4ee332c840287c63dd77
hackycy <qa894178522@qq.com>
2020-09-09 21:59:00 +0800

    chore：增加.prettierrc以便格式化

--- src/views/system/permission/components/user-dialog.vue
+++ src/views/system/permission/components/user-dialog.vue
@@ -1,108 +1,106 @@
 <template>
-  <div>
-    <el-dialog
-      :close-on-press-escape="false"
-      :close-on-click-modal="false"
-      :title="userAlertTitle"
-      :visible.sync="editerUserDialogVisible"
-      center
-      size="mini"
-      @open="handleUserDialogOpen"
-      @close="handleUserDialogClosed"
-    >
-      <div v-loading="isUserDialogLoading">
-        <el-form ref="userForm" :model="userForm" :rules="userFormRule">
-          <el-form-item label="头像" label-width="80px" prop="headImg">
-            <el-upload
-              action="/admin/upload/img"
-              list-type="picture-card"
-              :headers="getUploadheader()"
-              :before-upload="beforeAvatarUpload"
-              with-credentials
-              :limit="1"
-              :on-success="handleUploadAvatarSuccess"
-            >
-              <i class="el-icon-plus" />
-            </el-upload>
-          </el-form-item>
-          <el-form-item label="所属部门" label-width="80px" prop="departmentName" style="width: 100%;">
-            <el-popover placement="bottom-start" width="500">
-              <el-tree
-                node-key="id"
-                :data="deptTree.data"
-                :props="deptTree.props"
-                @node-click="handleSelectUserDeptNodeClick"
-              />
-              <el-input
-                slot="reference"
-                v-model="userForm.departmentName"
-                placeholder="请选择所属部门"
-                readonly
-              />
-            </el-popover>
-          </el-form-item>
-          <el-form-item label="所属角色" label-width="80px" prop="roles" style="width: 100%;">
-            <el-select
-              v-model="userForm.roles"
-              multiple
-              placeholder="请选择"
-              style="width: 100%;"
-              :multiple-limit="3"
-            >
-              <el-option v-for="item in roles" :key="item.id" :label="item.name" :value="item.id" />
-            </el-select>
-          </el-form-item>
-          <el-form-item label="账号" label-width="80px" prop="username">
-            <el-input v-model="userForm.username" placeholder="请输入账号" />
-          </el-form-item>
-          <el-row type="flex" justify="space-between">
-            <el-col :span="12">
-              <el-form-item label="姓名" label-width="80px" prop="name">
-                <el-input v-model="userForm.name" placeholder="请输入姓名" />
-              </el-form-item>
-            </el-col>
-            <el-col :span="12">
-              <el-form-item label="呢称" label-width="80px">
-                <el-input v-model="userForm.nickName" placeholder="请输入昵称" />
-              </el-form-item>
-            </el-col>
-          </el-row>
-          <el-row type="flex" justify="space-between">
-            <el-col :span="12">
-              <el-form-item label="邮箱" label-width="80px">
-                <el-input v-model="userForm.email" placeholder="请输入邮箱，用于获取初始密码" />
-              </el-form-item>
-            </el-col>
-            <el-col :span="12">
-              <el-form-item label="手机" label-width="80px">
-                <el-input v-model="userForm.phone" placeholder="请输入手机号码" />
-              </el-form-item>
-            </el-col>
-          </el-row>
-          <el-form-item label="备注" label-width="80px">
-            <el-input v-model="userForm.remark" type="textarea" />
-          </el-form-item>
-          <el-form-item label="状态类型" label-width="80px">
-            <el-radio-group v-model="userForm.status">
-              <el-radio :label="1">启用</el-radio>
-              <el-radio :label="0">禁用</el-radio>
-            </el-radio-group>
-          </el-form-item>
-        </el-form>
-      </div>
-      <div slot="footer">
-        <el-row type="flex" justify="end">
-          <el-button size="mini" @click="editerUserDialogVisible = false">取消</el-button>
-          <el-button
-            type="primary"
-            size="mini"
-            :loading="isUserSaveLoading"
-            @click="handleSaveUser"
-          >保存</el-button>
+  <el-dialog
+    :close-on-press-escape="false"
+    :close-on-click-modal="false"
+    :title="userAlertTitle"
+    :visible.sync="editerUserDialogVisible"
+    center
+    size="mini"
+    @open="handleUserDialogOpen"
+    @close="handleUserDialogClosed"
+  >
+    <div v-loading="isUserDialogLoading">
+      <el-form ref="userForm" :model="userForm" :rules="userFormRule">
+        <el-form-item label="头像" label-width="80px" prop="headImg">
+          <el-upload
+            action="/admin/upload/img"
+            list-type="picture-card"
+            :headers="getUploadheader()"
+            :before-upload="beforeAvatarUpload"
+            with-credentials
+            :limit="1"
+            :on-success="handleUploadAvatarSuccess"
+          >
+            <i class="el-icon-plus" />
+          </el-upload>
+        </el-form-item>
+        <el-form-item label="所属部门" label-width="80px" prop="departmentName" style="width: 100%;">
+          <el-popover placement="bottom-start" width="500">
+            <el-tree
+              node-key="id"
+              :data="deptTree.data"
+              :props="deptTree.props"
+              @node-click="handleSelectUserDeptNodeClick"
+            />
+            <el-input
+              slot="reference"
+              v-model="userForm.departmentName"
+              placeholder="请选择所属部门"
+              readonly
+            />
+          </el-popover>
+        </el-form-item>
+        <el-form-item label="所属角色" label-width="80px" prop="roles" style="width: 100%;">
+          <el-select
+            v-model="userForm.roles"
+            multiple
+            placeholder="请选择"
+            style="width: 100%;"
+            :multiple-limit="3"
+          >
+            <el-option v-for="item in roles" :key="item.id" :label="item.name" :value="item.id" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="账号" label-width="80px" prop="username">
+          <el-input v-model="userForm.username" placeholder="请输入账号" />
+        </el-form-item>
+        <el-row type="flex" justify="space-between">
+          <el-col :span="12">
+            <el-form-item label="姓名" label-width="80px" prop="name">
+              <el-input v-model="userForm.name" placeholder="请输入姓名" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="呢称" label-width="80px">
+              <el-input v-model="userForm.nickName" placeholder="请输入昵称" />
+            </el-form-item>
+          </el-col>
         </el-row>
-      </div>
-    </el-dialog>
-  </div>
+        <el-row type="flex" justify="space-between">
+          <el-col :span="12">
+            <el-form-item label="邮箱" label-width="80px">
+              <el-input v-model="userForm.email" placeholder="请输入邮箱，用于获取初始密码" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="手机" label-width="80px">
+              <el-input v-model="userForm.phone" placeholder="请输入手机号码" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-form-item label="备注" label-width="80px">
+          <el-input v-model="userForm.remark" type="textarea" />
+        </el-form-item>
+        <el-form-item label="状态类型" label-width="80px">
+          <el-radio-group v-model="userForm.status">
+            <el-radio :label="1">启用</el-radio>
+            <el-radio :label="0">禁用</el-radio>
+          </el-radio-group>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div slot="footer">
+      <el-row type="flex" justify="end">
+        <el-button size="mini" @click="editerUserDialogVisible = false">取消</el-button>
+        <el-button
+          type="primary"
+          size="mini"
+          :loading="isUserSaveLoading"
+          @click="handleSaveUser"
+        >保存</el-button>
+      </el-row>
+    </div>
+  </el-dialog>
 </template>
 
 <script>
@@ -112,7 +110,8 @@ import { filterDeptToTree } from '@/utils/permission'
 export default {
   name: 'SysUserDialog',
   props: {
-    mode: { // 新增模式 0 或 编辑模式 1
+    mode: {
+      // 新增模式 0 或 编辑模式 1
       type: Number,
       default: 0,
       validator: function(value) {
@@ -121,11 +120,11 @@ export default {
     },
     onSuccess: {
       type: Function,
-      default: () => { }
+      default: () => {}
     },
     onError: {
       type: Function,
-      default: () => { }
+      default: () => {}
     },
     visible: {
       type: Boolean,
@@ -172,7 +171,9 @@ export default {
       userFormRule: {
         name: [{ required: true, message: '请输入姓名', trigger: 'blur' }],
         username: [{ required: true, message: '请输入账号', trigger: 'blur' }],
-        departmentName: [{ required: true, message: '请选择所属部门', trigger: 'blur' }],
+        departmentName: [
+          { required: true, message: '请选择所属部门', trigger: 'blur' }
+        ],
         roles: [{ required: true, trigger: 'blur', validator: validateRoles }]
       }
     }
@@ -199,7 +200,7 @@ export default {
     },
     getUploadheader() {
       return {
-        'Authorization': getToken()
+        Authorization: getToken()
       }
     },
     beforeAvatarUpload(file) {
@@ -246,7 +247,7 @@ export default {
       }
     },
     handleSaveUser() {
-      this.$refs.userForm.validate(async valid => {
+      this.$refs.userForm.validate(async(valid) => {
         if (valid) {
           try {
             this.isUserSaveLoading = true
@@ -278,5 +279,4 @@ export default {
 }
 </script>
 
-<style>
-</style>
+<style></style>
