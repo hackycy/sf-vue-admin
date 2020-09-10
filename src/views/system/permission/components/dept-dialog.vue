faab5aaba3490e0743186b9fbfa2941d269701ad
hackycy <qa894178522@qq.com>
2020-09-10 22:37:14 +0800

    feature：copy code

---
+++ src/views/system/permission/components/dept-dialog.vue
@@ -0,0 +1,147 @@
+import request from '@/utils/request';
+<template>
+  <el-dialog
+    :close-on-press-escape="false"
+    :close-on-click-modal="false"
+    title="编辑"
+    :visible.sync="visible"
+    :before-close="dismiss"
+    center
+    size="mini"
+    @close="handleDialogClosed"
+  >
+    <div>
+      <el-form ref="deptForm" :model="deptForm" :rules="deptFormRule">
+        <el-form-item label="上级部门" label-width="80px" prop="departmentName" style="width: 100%;">
+          <el-popover placement="bottom-start" width="500">
+            <el-tree
+              node-key="id"
+              :data="deptTree.data"
+              :props="deptTree.props"
+              @node-click="handleSelectDeptNodeClick"
+            />
+            <el-input
+              slot="reference"
+              v-model="transferForm.departmentName"
+              placeholder="请选择所属部门"
+              readonly
+            />
+          </el-popover>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div slot="footer">
+      <el-row type="flex" justify="end">
+        <el-button size="mini" @click="dismiss">取消</el-button>
+        <el-button
+          type="primary"
+          size="mini"
+          :loading="isTransferLoading"
+          @click="handleTransferUser"
+        >转移</el-button>
+      </el-row>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+
+export default {
+  name: 'SysDeptTransferDialog',
+  props: {
+    deptTree: {
+      type: Object,
+      required: true
+    },
+    userIds: { // 需要获取的用户ID，编辑模式需要传入
+      type: Array,
+      default: function() {
+        return []
+      }
+    },
+    visible: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {
+      isTransferLoading: false,
+      // 部门
+      // deptTree: {
+      //   data: [],
+      //   props: {
+      //     children: 'children',
+      //     label: 'label'
+      //   }
+      // },
+      deptForm: {
+        departmentName: '',
+        departmentId: -1
+      },
+      deptFormRule: {
+        departmentName: [
+          { required: true, message: '请选择转移的部门', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    handleDialogClosed() {
+      this.transferForm = {
+        departmentName: '',
+        departmentId: -1
+      }
+      // if (this.$refs.userForm) {
+      //   this.$refs.userForm.this.$refs.roleForm.clearValidate()
+      // }
+    },
+    handleSelectDeptNodeClick(node) {
+      this.transferForm.departmentId = node.id
+      this.transferForm.departmentName = node.label
+    },
+    handleTransferUser() {
+      this.$refs.transferForm.validate(async(valid) => {
+        if (valid) {
+          try {
+            this.isTransferLoading = true
+            await this.$service.sys.dept.transfer({ userIds: this.userIds, departmentId: this.transferForm.departmentId })
+            this.isTransferLoading = false
+            this.$message({
+              message: '转移成功',
+              type: 'success'
+            })
+            this.success({ ...this.transferForm })
+          } catch (e) {
+            this.isTransferLoading = false
+            this.$message({
+              message: '转移失败',
+              type: 'warning'
+            })
+            this.fail()
+          }
+          this.dismiss()
+        } else {
+          this.$message({
+            message: '请正确填写内容',
+            type: 'warning'
+          })
+          return false
+        }
+      })
+    },
+    dismiss() {
+      // 父组件用于设置dialog隐藏dialog
+      this.$emit('dismiss')
+    },
+    success(data) {
+      this.$emit('success', data)
+    },
+    fail() {
+      this.$emit('fail')
+    }
+  }
+}
+</script>
+
+<style></style>
