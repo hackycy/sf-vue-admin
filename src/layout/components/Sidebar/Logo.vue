00fbe857fef4681efb0d9aaec1d378e904899ba5
hackycy <qa894178522@qq.com>
2020-09-13 01:22:11 +0800

    fix：更改Logo标题以及移除logo图标地址

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -24,8 +24,8 @@ export default {
   },
   data() {
     return {
-      title: 'Vue Element Admin',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      title: '思忆后台管理系统',
+      logo: ''
     }
   }
 }
