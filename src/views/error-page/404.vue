a0c65afa425e09e13b7a9a5e41e9544f3abbc92b
hackycy <qa894178522@qq.com>
2020-09-14 15:47:24 +0800

    styles：404页面改为中文

--- src/views/error-page/404.vue
+++ src/views/error-page/404.vue
@@ -9,12 +9,12 @@
       </div>
       <div class="bullshit">
         <div class="bullshit__oops">OOPS!</div>
-        <div class="bullshit__info">All rights reserved
-          <a style="color:#20a0ff" href="https://wallstreetcn.com" target="_blank">wallstreetcn</a>
+        <div class="bullshit__info">页面不见了～
+          <!-- <a style="color:#20a0ff" href="https://wallstreetcn.com" target="_blank">wallstreetcn</a> -->
         </div>
         <div class="bullshit__headline">{{ message }}</div>
-        <div class="bullshit__info">Please check that the URL you entered is correct, or click the button below to return to the homepage.</div>
-        <a href="" class="bullshit__return-home">Back to home</a>
+        <div class="bullshit__info">请检查您输入的URL是否正确，或单击下面的按钮返回主页。</div>
+        <a href="/" class="bullshit__return-home">返回首页</a>
       </div>
     </div>
   </div>
@@ -26,7 +26,7 @@ export default {
   name: 'Page404',
   computed: {
     message() {
-      return 'The webmaster said that you can not enter this page...'
+      return '您无法访问这个见面'
     }
   }
 }
