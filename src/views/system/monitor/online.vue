dc6b1a3b00c22eb2c80cb53689c153b7adc56547
hackycy <qa894178522@qq.com>
2020-10-12 23:53:06 +0800

    safe：权限按钮增加v-permission判定

--- src/views/system/monitor/online.vue
+++ src/views/system/monitor/online.vue
@@ -38,6 +38,7 @@
         <el-table-column label="操作" width="150" align="center" fixed="right">
           <template slot-scope="scope">
             <el-button
+              v-permission="$service.sys.online.permission.kick"
               size="mini"
               type="text"
               :disabled="scope.row.isCurrent"
