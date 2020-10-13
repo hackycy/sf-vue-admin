955b2576d3737c41c0e6ddafa26084a75e3418e0
hackycy <qa894178522@qq.com>
2020-10-13 22:59:23 +0800

    feat：完善登录日志页面

--- src/views/system/monitor/login-log.vue
+++ src/views/system/monitor/login-log.vue
@@ -15,25 +15,11 @@
         row-key="id"
         border
       >
-        <el-table-column prop="ip" label="请求IP" align="center" />
-        <el-table-column prop="userId" label="操作人ID" align="center" width="100" />
-        <el-table-column prop="method" label="请求方式" width="90" align="center">
-          <template slot-scope="scope">
-            <el-tag size="small" effect="dark">{{
-              scope.row.method
-            }}</el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column prop="params" show-overflow-tooltip label="请求参数" align="center" />
-        <el-table-column prop="action" show-overflow-tooltip label="请求地址" align="center" />
-        <el-table-column prop="status" label="响应状态" width="90" align="center">
-          <template slot-scope="scope">
-            <el-tag size="small" :type="getStatusType(scope.row.status)">{{
-              scope.row.status
-            }}</el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column prop="createTime" label="操作时间" align="center" />
+        <el-table-column prop="username" label="用户名" align="center" width="280" />
+        <el-table-column prop="ip" label="登录IP" align="center" />
+        <el-table-column prop="time" show-overflow-tooltip label="登陆时间" align="center" />
+        <el-table-column prop="os" show-overflow-tooltip label="操作系统" align="center" />
+        <el-table-column prop="browser" label="浏览器" align="center" />
       </el-table>
     </div>
     <div class="log-footer">
@@ -74,7 +60,7 @@ export default {
       const { data } = await this.$service.sys.loginLog.page({ page: this.currentPage, limit: this.pageSize })
       const { list, pagination } = data
       if (list && list.length > 0) {
-        this.logs = list.map(e => { e.createTime = momentParseTime(e.createTime); return e })
+        this.logs = list.map(e => { e.createTime = momentParseTime(e.time); return e })
         this.totalLogs = pagination.total
       } else {
         this.totalLogs = 0
