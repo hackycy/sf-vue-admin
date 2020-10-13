beea74885e016c4314ee1f777aae5380d4b8c0f2
hackycy <qa894178522@qq.com>
2020-10-13 18:03:02 +0800

    feat：增加登录日志路由map

--- src/router/index.js
+++ src/router/index.js
@@ -126,6 +126,7 @@ export const asyncRoutesMap = {
   'views/system/permission/role': () => import('@/views/system/permission/role'),
   'views/system/permission/user': () => import('@/views/system/permission/user'),
   'views/system/monitor/req-log': () => import('@/views/system/monitor/req-log'),
+  'views/system/monitor/login-log': () => import('@/views/system/monitor/login-log'),
   'views/system/monitor/online': () => import('@/views/system/monitor/online')
 }
 
