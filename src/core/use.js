c29de79841c255c93bf43316dd2fdd25bb45ed0e
小小源逼 <qa894178522@qq.com>
2021-03-28 00:20:55 +0800

    feat：define use.js

---
+++ src/core/use.js
@@ -0,0 +1,3 @@
+import ElementUI from 'element-ui'
+
+Vue.use(ElementUI)
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
-import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'// lang i18n
 
 import '@/styles/index.scss' // global css
@@ -11,10 +10,9 @@ import App from './App'
 import store from './store'
 import router from './router'
 
+import '@/core/use' // vue use
 import '@/icons' // icon
 import '@/permission' // permission control
-
-Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
