8756640df0b825ec430b044d2c5935531520ee84
hackycy <qa894178522@qq.com>
2021-04-02 11:35:37 +0800

    feat：add v-el-drag-dialog

--- src/core/use.js
+++ src/core/use.js
@@ -4,9 +4,12 @@ import ElementUI from 'element-ui'
 import PermissionHelper from '@/core/permission'
 import TableLayout from '@/layout/components/TableLayout'
 
+import ELDragDialog from '@/directive/el-drag-dialog'
+
 // use
 Vue.use(ElementUI)
 Vue.use(PermissionHelper)
+Vue.use(ELDragDialog)
 
 // component
 Vue.component('TableLayout', TableLayout)
