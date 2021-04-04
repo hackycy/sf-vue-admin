0ecb88bd1f580273771edb6ba2ad859c8f389d37
小小源逼 <qa894178522@qq.com>
2021-04-04 23:14:28 +0800

    feat：define PermissionCascader

---
+++ src/views/system/permission/components/permission-cascader.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-cascader
+    :value="value"
+    :options="options"
+    :props="props"
+    style="width: 100%;"
+    separator=":"
+    clearable
+    @change="handleChange"
+  />
+</template>
+
+<script>
+import PermissionMixin from '../../mixin/permission'
+
+export default {
+  name: 'PermissionCascader',
+  mixins: [PermissionMixin],
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['value'],
+  data() {
+    return {
+      props: { multiple: true },
+      options: []
+    }
+  },
+  created() {
+    const options = []
+    this.flatPerms().forEach(arr => {
+      this.filterPermToCascader(0, arr, options)
+    })
+    this.options = options
+  },
+  methods: {
+    /**
+     * using v-model
+     */
+    handleChange(v) {
+      this.$emit('change', v)
+    }
+  }
+}
+</script>
+
+<style></style>
