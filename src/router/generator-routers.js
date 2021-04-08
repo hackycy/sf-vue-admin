61a5f54e5f5f9e350c7b81e58a9d064559fddbc1
hackycy <qa894178522@qq.com>
2021-04-08 13:39:04 +0800

    refactor：封装createRoute方法

--- src/router/generator-routers.js
+++ src/router/generator-routers.js
@@ -5,19 +5,65 @@ import { toHump } from '@/utils'
 import { constantRouterComponents } from '@/router'
 
 /**
- * 获取外联路由
+ * 创建Route对象，需过滤权限节点
+ * @param {Object} menu 菜单
+ * @param {Boolean} isRoot 是否为根节点
  */
-export function getExternalLinkRoute(id, path, title, icon) {
-  return {
-    path: `external-link${id}`,
+function createRoute(menu, isRoot) {
+  // 目录
+  if (menu.type === 0) {
+    return {
+      path: menu.router,
+      component: isRoot ? Layout : BlankLayout,
+      alwaysShow: true,
+      meta: { title: menu.name, icon: menu.icon }
+    }
+  }
+  // 外链菜单
+  if (isExternal(menu.router)) {
+    return {
+      path: `external-link${menu.id}`,
+      component: Layout,
+      children: [
+        {
+          path: menu.router,
+          meta: { title: menu.name, icon: menu.icon }
+        }
+      ]
+    }
+  }
+  const component = constantRouterComponents[menu.viewPath]
+  if (!component) {
+    return undefined
+  }
+  // 根菜单
+  return isRoot ? {
+    path: menu.router,
+    redirect: `${menu.router}/index`,
     component: Layout,
     children: [
       {
-        path: path,
-        meta: { title, icon }
+        path: 'index',
+        name: toHump(menu.viewPath),
+        component,
+        meta: {
+          title: menu.name,
+          icon: menu.icon,
+          noCache: !menu.keepalive
+        }
       }
     ]
   }
+    : {
+      path: menu.router,
+      name: toHump(menu.viewPath),
+      component,
+      meta: {
+        title: menu.name,
+        icon: menu.icon,
+        noCache: !menu.keepalive
+      }
+    }
 }
 
 /**
@@ -37,74 +83,22 @@ export function filterAsyncRoutes(routes, parentRoute) {
     let realRoute
     if (!parentRoute && !route.parentId && route.type === 1) {
       // 根菜单
-      if (isExternal(route.router)) {
-        realRoute = getExternalLinkRoute(route.id, route.router, route.name, route.icon)
-      } else {
-        const component = constantRouterComponents[route.viewPath]
-        if (component) {
-          realRoute = {
-            path: route.router,
-            redirect: `${route.router}/index`,
-            component: Layout,
-            children: [
-              {
-                path: 'index',
-                name: toHump(route.viewPath),
-                component,
-                meta: {
-                  title: route.name,
-                  icon: route.icon,
-                  noCache: !route.keepalive
-                }
-              }
-            ]
-          }
-        }
-      }
+      realRoute = createRoute(route, true)
     } else if (!parentRoute && !route.parentId && route.type === 0) {
       // 目录
       const childRoutes = filterAsyncRoutes(routes, route)
-      realRoute = {
-        path: route.router,
-        component: Layout,
-        alwaysShow: true,
-        meta: { title: route.name, icon: route.icon }
-      }
+      realRoute = createRoute(route, true)
       if (childRoutes && childRoutes.length > 0) {
         realRoute.redirect = childRoutes[0].path
         realRoute.children = childRoutes
       }
     } else if (parentRoute && parentRoute.id === route.parentId && route.type === 1) {
       // 子菜单
-      if (isExternal(route.router)) {
-        realRoute = getExternalLinkRoute(route.id, route.router, route.name, route.icon)
-      } else {
-        const component = constantRouterComponents[route.viewPath]
-        if (component) {
-          realRoute = {
-            path: route.router,
-            name: toHump(route.viewPath),
-            meta: {
-              title: route.name,
-              icon: route.icon,
-              noCache: !route.keepalive
-            },
-            component
-          }
-        }
-      }
+      realRoute = createRoute(route, false)
     } else if (parentRoute && parentRoute.id === route.parentId && route.type === 0) {
       // 如果还是目录，继续递归
       const childRoute = filterAsyncRoutes(routes, route)
-      realRoute = {
-        path: route.router,
-        meta: {
-          title: route.name,
-          icon: route.icon
-        },
-        alwaysShow: true,
-        component: BlankLayout
-      }
+      realRoute = createRoute(route, false)
       if (childRoute && childRoute.length > 0) {
         realRoute.redirect = childRoute[0].path
         realRoute.children = childRoute
