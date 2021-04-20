e5cb5e738252569f874572f479ae7eda88f2441b
小小源逼 <qa894178522@qq.com>
2021-04-21 00:28:28 +0800

    refactor：remove finishTime column

--- src/views/system/schedule/log.vue
+++ src/views/system/schedule/log.vue
@@ -5,10 +5,15 @@
         <el-button size="mini" @click="handleRefresh">刷新</el-button>
       </template>
       <s-table ref="logTable" :data-request="getTaskLogList" show-pagination border>
-        <el-table-column prop="id" label="日志编号" align="center" width="120" />
-        <el-table-column prop="taskId" label="任务编号" align="center" width="120" />
+        <el-table-column prop="id" label="#" align="center" width="80" />
+        <el-table-column prop="taskId" label="任务编号" align="center" width="100" />
         <el-table-column prop="name" show-overflow-tooltip label="任务名称" align="center" width="200" />
         <el-table-column prop="detail" show-overflow-tooltip label="异常信息" align="center" />
+        <el-table-column prop="consumeTime" label="耗时" align="center" width="120">
+          <template slot-scope="scope">
+            <el-tag size="small" type="primary">{{ scope.row.consumeTime + ' ms' }}</el-tag>
+          </template>
+        </el-table-column>
         <el-table-column prop="status" label="状态" width="100" align="center">
           <template slot-scope="scope">
             <el-tag size="small" :type="getStatusType(scope.row.status)" effect="dark">{{
@@ -17,7 +22,6 @@
           </template>
         </el-table-column>
         <el-table-column prop="createTime" label="执行时间" align="center" width="220" />
-        <el-table-column prop="finishTime" label="完成时间" align="center" width="220" />
       </s-table>
     </table-layout>
   </div>
@@ -45,21 +49,17 @@ export default {
     getStatusType(status) {
       switch (status) {
         case 0:
-          return 'warning'
+          return 'danger'
         case 1:
           return 'success'
-        case 2:
-          return 'danger'
       }
     },
     getStatusTip(status) {
       switch (status) {
         case 0:
-          return '执行中'
+          return '失败'
         case 1:
           return '成功'
-        case 2:
-          return '失败'
       }
     }
   }
