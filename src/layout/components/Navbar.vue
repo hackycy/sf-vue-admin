f5d64d79cb60dea2671a94125ab23ece539c56c9
hackycy <qa894178522@qq.com>
2021-05-06 10:30:27 +0800

    styles：remove image handle

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -12,7 +12,7 @@
 
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <img :src="avatar" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
