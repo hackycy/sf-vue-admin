cc11a7a78a739c8ae483ca08bb4907c246497d7d
hackycy <qa894178522@qq.com>
2021-05-20 14:53:25 +0800

    fix：remove pollingLoading

--- src/views/netdisk/mixins/polling.js
+++ src/views/netdisk/mixins/polling.js
@@ -7,7 +7,6 @@ export default {
      */
     pollingCheckStatus(action, name, path, { success, fail }) {
       this.$message.success('已加入后台任务处理...请勿重复操作')
-      this.pollingLoading = true
       const val = setInterval(async() => {
         try {
           const { data } = await checkTaskStatus({
@@ -18,16 +17,13 @@ export default {
           if (data.status === 1) {
             setTimeout(() => {
               success && success()
-              this.pollingLoading = false
             }, 3000)
             clearInterval(val)
           } else if (data.status === 2) {
             fail && fail(data.err)
-            this.pollingLoading = false
             clearInterval(val)
           }
         } catch {
-          this.pollingLoading = false
           clearInterval(val)
         }
       }, 3000)
