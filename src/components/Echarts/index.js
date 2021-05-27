93da862ad77f1a049b6847e35db4097603190bc2
hackycy <qa894178522@qq.com>
2021-05-27 15:06:41 +0800

    feat：add trend chart

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,8 +1,15 @@
-import ECharts from 'vue-echarts' // 在 webpack 环境下指向 components/ECharts.vue
+import VueECharts from 'vue-echarts' // 在 webpack 环境下指向 components/ECharts.vue
 
+// https://github.com/ecomfe/vue-echarts/blob/5.x/README.zh_CN.md
 // 手动引入 ECharts 各模块来减小打包体积
 import 'echarts/lib/component/tooltip'
 import 'echarts/lib/component/toolbox'
+import 'echarts/lib/component/legend'
+import 'echarts/lib/component/dataset'
+import 'echarts/lib/component/grid'
+
+import 'echarts/lib/chart/line'
+import 'echarts/lib/chart/bar'
 
 // 如果需要配合 ECharts 扩展使用，只需要直接引入扩展包即可
 // 以 ECharts-GL 为例：
@@ -12,6 +19,6 @@ import 'echarts/lib/component/toolbox'
 export default {
   install(Vue) {
     // 注册组件后即可使用
-    Vue.component('v-chart', ECharts)
+    Vue.component('v-chart', VueECharts)
   }
 }
--- src/views/netdisk/overview.vue
+++ src/views/netdisk/overview.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="netdisk-overview-container">
-    <div class="ov-header">
+    <div v-loading="loading" class="ov-header">
       <overview-header-item
         title="存储总量"
         :value="spaceSize"
@@ -14,11 +14,25 @@
       />
       <overview-header-item title="GET请求次数" :value="hitSize" suffix="次" />
     </div>
-    <div class="ov-content">
+    <div v-loading="loading" class="ov-content">
       <el-card shadow="hover">
         <el-tabs v-model="actionTabName">
-          <el-tab-pane label="流量趋势" name="flow">流量趋势</el-tab-pane>
-          <el-tab-pane label="下载趋势" name="hit">下载趋势</el-tab-pane>
+          <el-tab-pane label="流量趋势" name="flow">
+            <v-chart
+              ref="flowChart"
+              :options="flowChartOption"
+              style="width: 100%; height: 320px;"
+              autoresize
+            />
+          </el-tab-pane>
+          <el-tab-pane label="容量趋势" name="space">
+            <v-chart
+              ref="spaceChart"
+              :options="spaceChartOption"
+              style="width: 100%; height: 320px;"
+              autoresize
+            />
+          </el-tab-pane>
         </el-tabs>
       </el-card>
     </div>
@@ -37,6 +51,7 @@ export default {
   },
   data() {
     return {
+      loading: false,
       // header
       spaceSize: 0,
       spaceUnit: 'B',
@@ -45,25 +60,159 @@ export default {
       flowUnit: 'B',
       hitSize: 0,
       // content
-      actionTabName: 'flow'
+      actionTabName: 'flow',
+      flowChartOption: {
+        tooltip: {
+          trigger: 'axis',
+          formatter: params => {
+            return `<div>${params[0].name}号</div><div>${params[0].marker} ${params[0].seriesName}：${params[0].value}MB</div>`
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          axisLine: {
+            show: false
+          },
+          splitLine: {
+            show: false
+          },
+          data: []
+        },
+        yAxis: {
+          type: 'value',
+          boundaryGap: false,
+          axisLabel: { formatter: '{value}MB' },
+          axisLine: {
+            show: false
+          },
+          axisTick: {
+            show: false
+          },
+          splitLine: {
+            show: false
+          }
+        },
+        grid: {
+          show: false,
+          top: '5%',
+          right: '2%',
+          left: '5%',
+          bottom: '8%'
+        },
+        series: [
+          {
+            name: '使用流量',
+            data: [],
+            type: 'line',
+            smooth: 0.6,
+            areaStyle: {
+              color: '#8cc6f2'
+            },
+            itemStyle: {
+              opacity: 0
+            },
+            lineStyle: {
+              color: '#8cc6f2'
+            }
+          }
+        ]
+      },
+      spaceChartOption: {
+        tooltip: {
+          trigger: 'axis',
+          formatter: params => {
+            return `<div>${params[0].name}号</div><div>${params[0].marker} ${params[0].seriesName}：${params[0].value}MB</div>`
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          axisLine: {
+            show: false
+          },
+          splitLine: {
+            show: false
+          },
+          data: []
+        },
+        yAxis: {
+          type: 'value',
+          boundaryGap: false,
+          axisLabel: { formatter: '{value}MB' },
+          axisLine: {
+            show: false
+          },
+          axisTick: {
+            show: false
+          },
+          splitLine: {
+            show: false
+          }
+        },
+        grid: {
+          show: false,
+          top: '5%',
+          right: '2%',
+          left: '5%',
+          bottom: '8%'
+        },
+        series: [
+          {
+            name: '占用空间',
+            data: [],
+            type: 'line',
+            smooth: 0.6,
+            areaStyle: {
+              color: '#8cc6f2'
+            },
+            itemStyle: {
+              opacity: 0
+            },
+            lineStyle: {
+              color: '#8cc6f2'
+            }
+          }
+        ]
+      }
     }
   },
-  created() {
-    this.initHeaderData()
+  mounted() {
+    this.initData()
   },
   methods: {
-    async initHeaderData() {
-      const { data } = await getDesc()
-      const sp = formatSizeUnits(data.spaceSize).split(' ')
-      this.spaceSize = Number(sp[0])
-      this.spaceUnit = sp[1]
-      this.fileSize = data.fileSize
-      const fs = formatSizeUnits(data.flowSize).split(' ')
-      this.flowSize = Number(fs[0])
-      this.flowUnit = fs[1]
-      this.hitSize = data.hitSize
+    async initData() {
+      try {
+        this.showLoading()
+        const { data } = await getDesc()
+        const sp = formatSizeUnits(data.spaceSize).split(' ')
+        this.spaceSize = Number(sp[0])
+        this.spaceUnit = sp[1]
+        this.fileSize = data.fileSize
+        const fs = formatSizeUnits(data.flowSize).split(' ')
+        this.flowSize = Number(fs[0])
+        this.flowUnit = fs[1]
+        this.hitSize = data.hitSize
+        // chart
+        this.flowChartOption.xAxis.data = data.flowTrend.times
+        this.flowChartOption.series[0].data = data.flowTrend.datas.map(e =>
+          (e / 1024 / 1024).toFixed()
+        )
+
+        this.spaceChartOption.xAxis.data = data.sizeTrend.times
+        this.spaceChartOption.series[0].data = data.sizeTrend.datas.map(e =>
+          (e / 1024 / 1024).toFixed()
+        )
+      } finally {
+        this.hideLoading()
+      }
     },
-    handleTabClick() {}
+    showLoading() {
+      this.loading = true
+    },
+    hideLoading() {
+      this.loading = false
+    }
   }
 }
 </script>
