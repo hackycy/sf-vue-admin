3d2f03b2d38f57a9dcd3d80b48f2ecfde2de9bc0
小小源逼 <qa894178522@qq.com>
2021-06-05 11:48:53 +0800

    feat：remove file space views

--- src/core/permission/modules/file/space.js
+++
@@ -1,9 +0,0 @@
-export default {
-  list: 'file/space/list',
-  mkdir: 'file/space/mkdir',
-  token: 'file/space/token',
-  rename: 'file/space/rename',
-  download: 'file/space/download',
-  delete: 'file/space/delete',
-  check: 'file/space/check'
-}
