2e2c45692235429e6972118c393cc36578a6cd0d
hackycy <qa894178522@qq.com>
2021-06-24 16:23:36 +0800

    docs：add note

--- vue.config.js
+++ vue.config.js
@@ -133,7 +133,7 @@ module.exports = {
                   test: resolve('src/components'), // can customize your rules
                   minChunks: 3, //  minimum common number
                   priority: 5,
-                  reuseExistingChunk: true
+                  reuseExistingChunk: true // 表示是否使用已有的 chunk，如果为 true 则表示如果当前的 chunk 包含的模块已经被抽取出去了，那么将不会重新生成新的。
                 }
               }
             })
