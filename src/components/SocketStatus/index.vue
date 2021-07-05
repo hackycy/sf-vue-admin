6478ea168eeab6329a770ce810b3bcb9778c3220
小小源逼 <qa894178522@qq.com>
2021-07-05 23:41:08 +0800

    feat：change socket status icon & add animation in connecting

--- src/components/SocketStatus/index.vue
+++ src/components/SocketStatus/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <svg-icon :icon-class="statusIcon" />
+    <svg-icon v-if="!isConnecting" :icon-class="statusIcon" />
+    <template v-else>
+      <div class="wifi-box">
+        <div class="center-box">
+          <svg-icon class="wifi-1" icon-class="socket-status-connecting1" />
+          <svg-icon class="wifi-2" icon-class="socket-status-connecting2" />
+          <svg-icon class="wifi-3" icon-class="socket-status-connecting3" />
+        </div>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -17,14 +26,66 @@ export default {
   },
   computed: {
     statusIcon() {
-      if (this.status === SocketStatus.CONNECTING) {
-        return 'socket-status-connecting'
-      } else if (this.status === SocketStatus.CONNECTED) {
+      if (this.status === SocketStatus.CONNECTED) {
         return 'socket-status-connected'
       } else {
         return 'socket-status-close'
       }
+    },
+    isConnecting() {
+      return this.status === SocketStatus.CONNECTING
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.wifi-box {
+  height: 100%;
+  width: 18px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .center-box {
+    position: relative;
+    width: 100%;
+    height: 18px;
+
+    .wifi-1,
+    .wifi-2,
+    .wifi-3 {
+      top: 0;
+      left: 0;
+      position: absolute;
+      display: block;
+    }
+
+    .wifi-3 {
+      animation: signal3 1s steps(1) infinite;
+    }
+
+    .wifi-2 {
+      animation: signal2 1s steps(1) infinite;
+    }
+  }
+}
+
+@keyframes signal3 {
+  0% {
+    opacity: 0;
+  }
+  30% {
+    opacity: 1;
+  }
+}
+
+@keyframes signal2 {
+  0% {
+    opacity: 0;
+  }
+  15% {
+    opacity: 1;
+  }
+}
+</style>
