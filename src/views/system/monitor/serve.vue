cbc6eb431c3f9beecaa44ca45cfde743a5882889
hackycy <qa894178522@qq.com>
2021-09-30 15:34:43 +0800

    feat: define serve view

---
+++ src/views/system/monitor/serve.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="sys-server-stat-container">
+    服务状态
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SystemMonitorServe'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
