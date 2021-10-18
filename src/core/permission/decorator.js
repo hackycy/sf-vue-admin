5dd98242172633e3dbae1b0e0d5161d9aea0cfb5
hackycy <qa894178522@qq.com>
2021-10-18 14:25:56 +0800

    feat: impl PermissionAction and PermissionPrefix decorator

--- src/core/permission/decorator.js
+++ src/core/permission/decorator.js
@@ -1,4 +1,37 @@
+import { isString } from 'lodash'
+
 /**
- * 权限装饰器
+ * 权限点，例如 add
+ * 会与prefix 拼接组成 例如：sys/user/add
  */
-export function Permission() {}
+export function PermissionAction(action) {
+  return function(target, propertyKey, _) {
+    if (!isString(action)) {
+      throw new Error('unsupport permission action type')
+    }
+
+    if (!target.$permission) {
+      target.$permission = {}
+    }
+
+    // 延迟执行
+    setTimeout(() => {
+      // 挂载权限点
+      target.$permission[propertyKey] =
+        (`${target.$permissonPrefix ? target.$permissonPrefix + '/' : ''}${action}`).replace(/\//g, ':')
+    }, 0)
+  }
+}
+
+/**
+ * 权限前缀，例如 sys/user
+ */
+export function PermissionPrefix(prefix = '') {
+  return function(constructor) {
+    if (!isString(prefix)) {
+      throw new Error('unsupport permission prefix type')
+    }
+
+    constructor.prototype.$permissonPrefix = prefix
+  }
+}
